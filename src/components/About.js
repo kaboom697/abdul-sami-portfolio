ca352199a3d5a82217c7eb22536e333622b2d657
kaboom697 <112824460+kaboom697@users.noreply.github.com>
2025-11-24 20:37:55 +0400

    Initial Commit
    Uploading the completed project, first version.

---
+++ src/components/About.js
@@ -0,0 +1,107 @@
+import React, { Component } from "react";
+import { Icon } from "@iconify/react";
+import angularIcon from "@iconify/icons-logos/angular-icon";
+import reactIcon from "@iconify/icons-logos/react";
+import vueIcon from "@iconify/icons-logos/vue";
+
+class About extends Component {
+  render() {
+    if (this.props.sharedBasicInfo) {
+      var profilepic = "images/" + this.props.sharedBasicInfo.image;
+    }
+    if (this.props.resumeBasicInfo) {
+      var sectionName = this.props.resumeBasicInfo.section_name.about;
+      var hello = this.props.resumeBasicInfo.description_header;
+      var about = this.props.resumeBasicInfo.description;
+    }
+
+    return (
+      <section id="about">
+        <div className="col-md-12">
+          <h1 style={{ color: "black" }}>
+            <span>{sectionName}</span>
+          </h1>
+          <div className="row center mx-auto mb-5">
+            <div className="col-md-4 mb-5 center">
+              <div className="polaroid">
+                <span style={{ cursor: "auto" }}>
+                  <img
+                    height="250px"
+                    src={profilepic}
+                    alt="Avatar placeholder"
+                  />
+                </span>
+              </div>
+            </div>
+
+            <div className="col-md-8 center">
+              <div className="col-md-10">
+                <div className="card">
+                  <div className="card-header">
+                    <span
+                      className="iconify"
+                      data-icon="emojione:red-circle"
+                      data-inline="false"
+                    ></span>{" "}
+                    &nbsp;{" "}
+                    <span
+                      className="iconify"
+                      data-icon="twemoji:yellow-circle"
+                      data-inline="false"
+                    ></span>{" "}
+                    &nbsp;{" "}
+                    <span
+                      className="iconify"
+                      data-icon="twemoji:green-circle"
+                      data-inline="false"
+                    ></span>
+                  </div>
+                  <div
+                    className="card-body font-trebuchet text-justify ml-3 mr-3"
+                    style={{
+                      height: "auto",
+                      fontSize: "132%",
+                      lineHeight: "200%",
+                    }}
+                  >
+                    <br />
+                    <span className="wave">{hello}</span>
+                    <br />
+                    <br />
+                    {about}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-12 text-center mt-4">
+
+          {/* Download PDF button */}
+          <a
+            href="/documents/Abdul Sami CV.pdf"
+            download
+            className="download-btn"
+          >
+            Download Resume
+          </a>
+
+          {/* View on Google Drive link */}
+          <div className="view-link">
+            <a
+              href="https://drive.google.com/file/d/1APdW4yW1bmNbcRs8CIIaTJ_sqGZzvJkN/view?usp=sharing"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              View Online (Google Drive)
+            </a>
+          </div>
+
+        </div>
+
+      </section>
+    );
+  }
+}
+
+export default About;
